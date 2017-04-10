be7ba592b7e53051f76434466e56b222025ae2b8
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-04-10 16:07:29 +0800

    web样式调整

--- src/main/webapp/dist/js/functions.js
+++ src/main/webapp/dist/js/functions.js
@@ -6,3 +6,31 @@ function requestData(requestContent, callBack) {
 		callBack(data);
 	});
 }
+
+function getHome() {
+	requestData({
+		originType : "1",
+		requestContent : "管家"
+	}, function(data) {
+		$("#main-content").html(data);
+	});
+}
+
+$(document).ready(function() {
+
+	setTimeout("getHome();", 2000);
+
+	$("#main-search-input").keyup(function(event) {
+		var input = $(this);
+		if (event.keyCode == 13) {
+			requestData({
+				originType : "1",
+				requestContent : input.val()
+			}, function(data) {
+				$("#main-content").append(data);
+				input.val("");
+			});
+		}
+	});
+
+});
