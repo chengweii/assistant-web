395d3a309bf4651d57a1da78e799632599a058d6
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-04-18 17:50:55 +0800

    基础完善

--- src/main/webapp/dist/js/functions.js
+++ src/main/webapp/dist/js/functions.js
@@ -1,36 +1,108 @@
-function requestData(requestContent, callBack) {
-	var content = JSON.stringify(requestContent);
-	$.post("request.htm", {
-		requestContent : content
-	}, function(data) {
-		callBack(data);
-	});
-}
+window.assistant = {
+	requestData : function(requestContent, callBack) {
+		requestContent.originType = "1";
+		var content = JSON.stringify(requestContent);
+		$.post("request.htm", {
+			requestContent : content
+		}, function(data) {
+			callBack(data);
+		});
+	},
+	getHome : function() {
+		assistant.requestData({
+			requestContent : "管家"
+		}, function(data) {
+			$("#main-content").html(data);
+		});
+	},
+	keepAssistantContainerUnique : function(styleClass) {
+		var size = $("." + styleClass).size();
+		$("." + styleClass).each(function(index, item) {
+			if (index < size - 1) {
+				$(item).remove();
+			}
+		});
+	},
+	modal : {
+		showDialog : function(params) {
+			if (params.title)
+				$('#modalDefault .modal-title').html(params.title);
+			if (params.okText)
+				$('#modalDefault .btn-ok').html(params.okText);
+			if (params.cancelText)
+				$('#modalDefault .btn-cancel').html(params.cancelText);
+			if (params.content)
+				$('#modalDefault .modal-body').html(params.content);
+			if (params.height)
+				$('#modalDefault .modal-body').css("height",
+						params.height + "px");
+			if (params.width)
+				$('#modalDefault .modal-dialog').css("width",
+						params.width + "px");
+			if (params.modalType == '1') {
+				$('#modalDefault .modal-footer').hide();
+				if (params.hideCallback)
+					$('#modalDefault').on('hidden.bs.modal',
+							params.hideCallback);
+			} else if (params.modalType == '2') {
+				if (params.cancelCallback)
+					$('#modalDefault .btn-cancel').unbind().click(
+							params.cancelCallback);
+				if (params.okCallback)
+					$('#modalDefault .btn-ok').unbind()
+							.click(params.okCallback);
+			}
 
-function getHome() {
-	requestData({
-		originType : "1",
-		requestContent : "管家"
-	}, function(data) {
-		$("#main-content").html(data);
-	});
-}
-
-$(document).ready(function() {
-
-	setTimeout("getHome();", 2000);
-
-	$("#main-search-input").keyup(function(event) {
-		var input = $(this);
-		if (event.keyCode == 13) {
-			requestData({
-				originType : "1",
-				requestContent : input.val()
-			}, function(data) {
-				$("#main-content").append(data);
-				input.val("");
+			$('#modalDefault').modal('show');
+		},
+		alert : function(msg, hideCallback) {
+			assistant.modal.showDialog({
+				modalType : 1,
+				content : msg,
+				hideCallback : hideCallback,
+				width : 200,
+				height : 50
+			});
+		},
+		confirm : function(msg, okCallback, cancelCallback) {
+			assistant.modal.showDialog({
+				modalType : 2,
+				content : msg,
+				okCallback : okCallback,
+				cancelCallback : cancelCallback,
+				width : 200,
+				height : 50
 			});
 		}
-	});
+	}
+};
 
-});
+$(document).ready(
+		function() {
+
+			setTimeout("assistant.getHome();", 1000);
+
+			$("#main-search-input").keyup(
+					function(event) {
+						var input = $(this);
+						if (event.keyCode == 13) {
+							var value = input.val();
+							if (!value || $.trim(value) == '') {
+								assistant.modal.alert(
+										"Please say something at least.",
+										function() {
+											input.val("");
+											input.focus();
+										});
+								return;
+							}
+							assistant.requestData({
+								requestContent : value
+							}, function(data) {
+								$("#main-content").append(data);
+								input.val("");
+							});
+						}
+					});
+
+		});
