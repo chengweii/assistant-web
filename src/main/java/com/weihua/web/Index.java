60bee4f1617c03157bc0b90d7476cc903d67cf1a
chengwei2 <chengwei2@JXWBJ1207261100.jiuxian.local>
2017-04-20 18:40:17 +0800

    服务优化

--- src/main/java/com/weihua/web/Index.java
+++ src/main/java/com/weihua/web/Index.java
@@ -8,35 +8,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-
 import com.weihua.assistant.entity.request.BaseRequest;
 import com.weihua.common.constant.CommonConstant;
 import com.weihua.ui.userinterface.AssistantInterface;
 import com.weihua.ui.userinterface.UserInterface;
-import com.weihua.util.DBUtil;
 import com.weihua.util.TemplateUtil;
-import com.weihua.web.util.WebDBHelper;
-import com.weihua.web.util.WebTemplateReader;
 
 public class Index extends HttpServlet {
 
-	private static Logger LOGGER = Logger.getLogger(Index.class);
-
 	private static final long serialVersionUID = 1L;
 
-	private static UserInterface userInterface = new AssistantInterface();
-
-	private static String webRootPath = null;
+	private static final UserInterface USER_INTERFACE = new AssistantInterface();
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=" + CommonConstant.CHARSET_UTF8);
 		String requestContent = request.getParameter(BaseRequest.REQUEST_PARAM_KEY);
 		String content = null;
 		if (requestContent == null) {
-			content = TemplateUtil.renderByTemplateFile(webRootPath, "index.htm", null);
+			content = TemplateUtil.renderByTemplateFile(this.getServletContext().getRealPath("/"), "index.htm", null);
 		} else {
-			content = userInterface.getResponse(requestContent);
+			content = USER_INTERFACE.getResponse(requestContent);
 		}
 
 		PrintWriter out = response.getWriter();
@@ -48,13 +39,6 @@ public class Index extends HttpServlet {
 	}
 
 	public void destroy() {
-	}
-
-	public void init() throws ServletException {
-		webRootPath = this.getServletContext().getRealPath("/");
-		LOGGER.info("webRootPath:" + webRootPath);
-		TemplateUtil.initTemplateReader(new WebTemplateReader());
-		DBUtil.initDBHelper(new WebDBHelper(webRootPath));
 	}
 
 }
---
+++ src/main/java/com/weihua/web/util/AlarmService.java
@@ -0,0 +1,97 @@
+package com.weihua.web.util;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.ServletException;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.reflect.TypeToken;
+import com.weihua.assistant.entity.request.BaseRequest;
+import com.weihua.ui.userinterface.AssistantInterface;
+import com.weihua.ui.userinterface.UserInterface;
+import com.weihua.util.DBUtil;
+import com.weihua.util.ExceptionUtil;
+import com.weihua.util.GsonUtil;
+import com.weihua.util.TemplateUtil;
+
+public class AlarmService implements ServletContextListener {
+	private static final Logger LOGGER = Logger.getLogger(AlarmService.class);
+	private static Timer timer = null;
+	private static final int DELAY = 5000;
+	private static final int PERIOD = 1000;
+
+	private static final UserInterface USER_INTERFACE = new AssistantInterface();
+	private static final String ALARM_REQUEST_CONTENT;
+
+	static {
+		BaseRequest.RequestData request = new BaseRequest.RequestData();
+		request.isLocationPath = true;
+		request.requestContent = "callAlarmService";
+		ALARM_REQUEST_CONTENT = GsonUtil.toJson(request);
+	}
+
+	public AlarmService() {
+		super();
+	}
+
+	private void init(ServletContextEvent servletContextEvent) {
+		if (timer == null) {
+			timer = new Timer();
+			String webRootPath = servletContextEvent.getServletContext().getRealPath("/");
+			LOGGER.info("webRootPath:" + webRootPath);
+			TemplateUtil.initTemplateReader(new WebTemplateReader());
+			DBUtil.initDBHelper(new WebDBHelper(webRootPath));
+		}
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent servletContextEvent) {
+		servletContextEvent.getServletContext().log("AlarmService is starting.");
+		init(servletContextEvent);
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				try {
+					String responseContent = USER_INTERFACE.getResponse(ALARM_REQUEST_CONTENT);
+					Map<String, Object> alarm = GsonUtil.getEntityFromJson(responseContent,
+							new TypeToken<Map<String, Object>>() {
+							});
+					if (alarm != null && alarm.get("msgList") != null) {
+						List<Map<String, String>> alarmList = GsonUtil.getEntityFromJson(
+								alarm.get("msgList").toString(), new TypeToken<List<Map<String, String>>>() {
+								});
+						for (Map<String, String> item : alarmList) {
+							showMsg(item);
+						}
+					}
+				} catch (Exception e) {
+					LOGGER.error("AlarmService run error:", e);
+				}
+			}
+		}, DELAY, PERIOD);
+		servletContextEvent.getServletContext().log("AlarmService was started.");
+	}
+
+	private void showMsg(Map<String, String> msg) {
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			runtime.exec("cmd /c mshta vbscript:msgbox(\"" + msg.get("content") + "\",64,\"" + msg.get("title")
+					+ "\")(window.close)");
+		} catch (Exception e) {
+			LOGGER.error("Show msg error.", e);
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent servletContextEvent) {
+		timer.cancel();
+		servletContextEvent.getServletContext().log("AlarmService was stopped.");
+	}
+
+}
