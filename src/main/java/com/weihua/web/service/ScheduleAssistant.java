20eb6938c8526ddacb747c91a8e87d408e94377a
chengwei2 <chengwei2@jiuxian.com>
2018-04-28 11:51:50 +0800

    update

--- src/main/java/com/weihua/web/service/ScheduleAssistant.java
+++ src/main/java/com/weihua/web/service/ScheduleAssistant.java
@@ -1,26 +1,15 @@
 package com.weihua.web.service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
-
+import com.google.common.base.Strings;
 import com.google.common.base.Throwables;
-import com.weihua.common.util.CollectionUtil;
-import com.weihua.common.util.DateUtil;
+import com.weihua.common.util.*;
 import com.weihua.common.util.DateUtil.DateFormatType;
 import com.weihua.common.util.DateUtil.TimePeriod;
-import com.weihua.common.util.DidaListUtil;
 import com.weihua.common.util.DidaListUtil.Task;
+import com.weihua.common.util.DidaListUtil.Task.Item;
 import com.weihua.common.util.DidaListUtil.TaskStatus;
 import com.weihua.common.util.DidaListUtil.TaskType;
-import com.weihua.common.util.DidaListUtil.Task.Item;
-import com.weihua.common.util.EmailUtil;
 import com.weihua.common.util.EmailUtil.SendInfo;
-import com.weihua.common.util.GsonUtil;
 import com.weihua.web.constant.AssistantConstant;
 import com.weihua.web.constant.OriginType;
 import com.weihua.web.dao.FamilyDao;
@@ -36,6 +25,9 @@ import com.weihua.web.service.annotation.BackServiceLocation;
 import com.weihua.web.service.annotation.ServiceLocation;
 import com.weihua.web.service.base.BaseAssistant;
 import com.weihua.web.util.ConfigUtil;
+import org.apache.log4j.Logger;
+
+import java.util.*;
 
 /**
  * @author chengwei2
@@ -43,129 +35,133 @@ import com.weihua.web.util.ConfigUtil;
  */
 public class ScheduleAssistant extends BaseAssistant {
 
-	private static Logger LOGGER = Logger.getLogger(ScheduleAssistant.class);
-	private static LifeMotoDao lifeMotoDao = new LifeMotoDaoImpl();
-	private static HolidayArrangementDao holidayArrangementDao = new HolidayArrangementDaoImpl();
-	private static FamilyDao familyDao = new FamilyDaoImpl();
+    private static Logger LOGGER = Logger.getLogger(ScheduleAssistant.class);
+    private static LifeMotoDao lifeMotoDao = new LifeMotoDaoImpl();
+    private static HolidayArrangementDao holidayArrangementDao = new HolidayArrangementDaoImpl();
+    private static FamilyDao familyDao = new FamilyDaoImpl();
 
-	@Override
-	public Response getResponse(Request request) {
-		Response response = null;
-		try {
-			BaseRequest baseRequest = (BaseRequest) request;
-			if (baseRequest.isLocationPath() == null || baseRequest.isLocationPath() == false) {
-				analysisRequest(baseRequest);
-				response = getScheduleList((BaseRequest) request);
-			} else {
-				response = invokeLocationMethod((BaseRequest) request);
-			}
-		} catch (Exception e) {
-			Throwables.propagate(e);
-		}
-		return response;
-	}
+    @Override
+    public Response getResponse(Request request) {
+        Response response = null;
+        try {
+            BaseRequest baseRequest = (BaseRequest) request;
+            if (baseRequest.isLocationPath() == null || baseRequest.isLocationPath() == false) {
+                analysisRequest(baseRequest);
+                response = getScheduleList((BaseRequest) request);
+            } else {
+                response = invokeLocationMethod((BaseRequest) request);
+            }
+        } catch (Exception e) {
+            Throwables.propagate(e);
+        }
+        return response;
+    }
 
-	private void analysisRequest(BaseRequest baseRequest) {
-		String content = baseRequest.getContent();
-		Map<String, String> extraInfo = new HashMap<String, String>();
-		TaskType taskType = TaskType.fromValue(content);
-		if (taskType != null) {
-			extraInfo.put("taskType", taskType.getCode());
-			baseRequest.setExtraInfo(GsonUtil.toJson(extraInfo));
-		}
-	}
+    private void analysisRequest(BaseRequest baseRequest) {
+        String content = baseRequest.getContent();
+        Map<String, String> extraInfo = new HashMap<String, String>();
+        TaskType taskType = TaskType.fromValue(content);
+        if (taskType != null) {
+            extraInfo.put("taskType", taskType.getCode());
+            baseRequest.setExtraInfo(GsonUtil.toJson(extraInfo));
+        }
+    }
 
-	@ServiceLocation(value = "getScheduleList")
-	public Response getScheduleList(BaseRequest request) throws Exception {
-		if (request.getOriginType() != OriginType.WEB)
-			return null;
-		String extraInfo = request.getExtraInfo();
-		Map<String, String> extraInfoMap = GsonUtil.getMapFromJson(extraInfo);
+    @ServiceLocation(value = "getScheduleList")
+    public Response getScheduleList(BaseRequest request) throws Exception {
+        if (request.getOriginType() != OriginType.WEB)
+            return null;
+        String extraInfo = request.getExtraInfo();
+        Map<String, String> extraInfoMap = GsonUtil.getMapFromJson(extraInfo);
 
-		TaskType taskType = TaskType.fromCode(String.valueOf(extraInfoMap.get("taskType")));
-		List<Task> taskList = DidaListUtil.getTaskListFromDida365(taskType, TaskStatus.UNFINISH);
+        TaskType taskType = TaskType.fromCode(String.valueOf(extraInfoMap.get("taskType")));
+        List<Task> taskList = DidaListUtil.getTaskListFromDida365(taskType, TaskStatus.UNFINISH);
 
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("taskCode", taskType.getCode());
-		model.put("taskValue", taskType.getValue());
-		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("taskCode", taskType.getCode());
+        model.put("taskValue", taskType.getValue());
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
-		if (CollectionUtil.isNotEmpty(taskList)) {
-			for (Task item : taskList) {
-				Map<String, Object> entity = new HashMap<String, Object>();
-				entity.put("id", item.id);
-				entity.put("title", item.title);
-				entity.put("status", item.status);
-				list.add(entity);
-			}
-		}
+        if (CollectionUtil.isNotEmpty(taskList)) {
+            for (Task item : taskList) {
+                Map<String, Object> entity = new HashMap<String, Object>();
+                entity.put("id", item.id);
+                entity.put("title", item.title);
+                entity.put("status", item.status);
+                list.add(entity);
+            }
+        }
 
-		model.put("taskList", list);
+        model.put("taskList", list);
 
-		return response(model, "schedule/tasklist");
-	}
+        return response(model, "schedule/tasklist");
+    }
 
-	@BackServiceLocation(value = "getScheduleByTime")
-	public Response getScheduleByTime(BaseRequest request) throws Exception {
-		Map<String, Object> model = new HashMap<String, Object>();
-		if (isTriggerServiceReminding(request)) {
-			bindScheduleModel(model);
+    @BackServiceLocation(value = "getScheduleByTime")
+    public Response getScheduleByTime(BaseRequest request) throws Exception {
+        Map<String, Object> model = new HashMap<String, Object>();
+        if (isTriggerServiceReminding(request)) {
+            bindScheduleModel(model);
 
-			SendInfo info = new SendInfo();
-			info.setReceiveUser(ConfigUtil.get(AssistantConstant.FAMILY_ASSISTANT_EMAIL_SCHEDULE_REMINDEMAILUSER));
-			info.setHeadName(AssistantConstant.FAMILY_ASSISTANT_STRING_12);
-			Response response = response(model, "schedule/schedule");
-			info.setSendHtml(response.getContent());
-			EmailUtil.send(info);
+            SendInfo info = new SendInfo();
+            info.setReceiveUser(ConfigUtil.get(AssistantConstant.FAMILY_ASSISTANT_EMAIL_SCHEDULE_REMINDEMAILUSER));
+            info.setHeadName(AssistantConstant.FAMILY_ASSISTANT_STRING_12);
+            Response response = response(model, "schedule/schedule");
+            info.setSendHtml(response.getContent());
+            EmailUtil.send(info);
 
-			return responseJson(null);
-		}
+            return responseJson(null);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	private void bindScheduleModel(Map<String, Object> model) throws Exception {
-		Map<String, Object> lifeMoto = lifeMotoDao.findRandomRecord();
-		model.put("lifeMoto", lifeMoto.get("moto"));
-		model.put("lifeMotoImage", lifeMoto.get("collocation_picture"));
+    private void bindScheduleModel(Map<String, Object> model) throws Exception {
+        Map<String, Object> lifeMoto = lifeMotoDao.findRandomRecord();
+        model.put("lifeMoto", lifeMoto.get("moto"));
+        model.put("lifeMotoImage", lifeMoto.get("collocation_picture"));
 
-		List<Map<String, Object>> scheduleList = new ArrayList<Map<String, Object>>();
-		List<Task> taskList = DidaListUtil.getTaskListFromDida365(TaskType.CURRENT_SCHEDULE, TaskStatus.UNFINISH);
-		if (!CollectionUtil.isEmpty(taskList)) {
-			for (Task task : taskList) {
-				Map<String, Object> map = new HashMap<String, Object>();
-				map.put("schedule_time", task.startDate);
-				map.put("schedule_title", task.title);
-				map.put("schedule_content", task.content);
-				scheduleList.add(map);
-			}
-		}
+        List<Map<String, Object>> scheduleList = new ArrayList<Map<String, Object>>();
+        List<Task> taskList = DidaListUtil.getTaskListFromDida365(TaskType.CURRENT_SCHEDULE, TaskStatus.UNFINISH);
+        if (!CollectionUtil.isEmpty(taskList)) {
+            for (Task task : taskList) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                if (Strings.isNullOrEmpty(task.startDate)) {
+                    map.put("schedule_title", task.title);
+                } else {
+                    Date date = DateUtil.getDateFormat(task.startDate, DateFormatType.YYYY_MM_DD_T_HH_MM_SS_SSSZ);
+                    map.put("schedule_title", task.title + "<br>" + DateUtil.getDateFormat(date, DateFormatType.YYYY_MM_DD_HH_MM_SS));
+                }
+                map.put("schedule_content", task.content);
+                scheduleList.add(map);
+            }
+        }
 
-		boolean isHoliday = holidayArrangementDao
-				.findIsHoliday(DateUtil.getDateFormat(new Date(), DateFormatType.YYYY_MM_DD));
-		List<Task> triflesTaskList = DidaListUtil.getTaskListFromDida365(TaskType.MY_DAILY, TaskStatus.UNFINISH);
-		if (!CollectionUtil.isEmpty(triflesTaskList)) {
-			String prefix = isHoliday ? AssistantConstant.FAMILY_ASSISTANT_STRING_6
-					: AssistantConstant.FAMILY_ASSISTANT_STRING_7;
-			List<Item> items = null;
-			for (Task task : triflesTaskList) {
-				if (task.title.contains(prefix)) {
-					items = task.items;
-				}
-			}
+        boolean isHoliday = holidayArrangementDao
+                .findIsHoliday(DateUtil.getDateFormat(new Date(), DateFormatType.YYYY_MM_DD));
+        List<Task> triflesTaskList = DidaListUtil.getTaskListFromDida365(TaskType.MY_DAILY, TaskStatus.UNFINISH);
+        if (!CollectionUtil.isEmpty(triflesTaskList)) {
+            String prefix = isHoliday ? AssistantConstant.FAMILY_ASSISTANT_STRING_6
+                    : AssistantConstant.FAMILY_ASSISTANT_STRING_7;
+            List<Item> items = null;
+            for (Task task : triflesTaskList) {
+                if (task.title.contains(prefix)) {
+                    items = task.items;
+                }
+            }
 
-			for (Item item : items) {
-				Map<String, Object> map = new HashMap<String, Object>();
-				String[] schedule = item.title.split("##");
-				map.put("schedule_time", schedule[0]);
-				map.put("schedule_title", schedule[1]);
-				map.put("schedule_content", schedule[2]);
-				scheduleList.add(map);
-			}
-		}
+            for (Item item : items) {
+                Map<String, Object> map = new HashMap<String, Object>();
+                String[] schedule = item.title.split("##");
+                map.put("schedule_time", schedule[0]);
+                map.put("schedule_title", schedule[1]);
+                map.put("schedule_content", schedule[2]);
+                scheduleList.add(map);
+            }
+        }
 
 		/*
-		 * List<Map<String, Object>> triflesList =
+         * List<Map<String, Object>> triflesList =
 		 * familyDao.findRecordListByTime("00:00", "23:59", isHoliday ?
 		 * AssistantConstant.FAMILY_ASSISTANT_STRING_6 :
 		 * AssistantConstant.FAMILY_ASSISTANT_STRING_7); if
@@ -177,49 +173,49 @@ public class ScheduleAssistant extends BaseAssistant {
 		 * scheduleList.add(map); } }
 		 */
 
-		Date tempDate;
-		TimePeriod timePeriod;
-		for (Map<String, Object> item : scheduleList) {
-			if (item.get("schedule_time") == null) {
-				item.put("schedule_time", "00:00");
-			}
-			tempDate = DateUtil.getDateFormat(String.valueOf(item.get("schedule_time")), DateFormatType.HH_MM);
-			timePeriod = DateUtil.getTimePeriodByDate(tempDate);
-			item.put("time_period_color", TimePeriodColor.fromCode(timePeriod.getCode()).getValue());
-		}
+        Date tempDate;
+        TimePeriod timePeriod;
+        for (Map<String, Object> item : scheduleList) {
+            if (item.get("schedule_time") == null) {
+                item.put("schedule_time", "00:00");
+            }
+            tempDate = DateUtil.getDateFormat(String.valueOf(item.get("schedule_time")), DateFormatType.HH_MM);
+            timePeriod = DateUtil.getTimePeriodByDate(tempDate);
+            item.put("time_period_color", TimePeriodColor.fromCode(timePeriod.getCode()).getValue());
+        }
 
-		model.put("scheduleList", scheduleList);
-	}
+        model.put("scheduleList", scheduleList);
+    }
 
-	public static enum TimePeriodColor {
-		MORNING("MORNING", "rgba(46,204,113,0.6)"), BEFORENOON("BEFORENOON", "rgba(26,188,156,0.6)"), NOON("NOON",
-				"rgba(231,76,60,0.6)"), AFTERNOON("AFTERNOON", "rgba(230,126,34,0.6)"), NIGHT("NIGHT",
-						"rgba(243,156,18,0.6)"), DEEPNIGHT("DEEPNIGHT", "rgba(192,57,43,0.6)");
+    public static enum TimePeriodColor {
+        MORNING("MORNING", "rgba(46,204,113,0.6)"), BEFORENOON("BEFORENOON", "rgba(26,188,156,0.6)"), NOON("NOON",
+                "rgba(231,76,60,0.6)"), AFTERNOON("AFTERNOON", "rgba(230,126,34,0.6)"), NIGHT("NIGHT",
+                "rgba(243,156,18,0.6)"), DEEPNIGHT("DEEPNIGHT", "rgba(192,57,43,0.6)");
 
-		private TimePeriodColor(String code, String value) {
-			this.code = code;
-			this.value = value;
-		}
+        private TimePeriodColor(String code, String value) {
+            this.code = code;
+            this.value = value;
+        }
 
-		private String code;
-		private String value;
+        private String code;
+        private String value;
 
-		public String getCode() {
-			return code;
-		}
+        public String getCode() {
+            return code;
+        }
 
-		public String getValue() {
-			return value;
-		}
+        public String getValue() {
+            return value;
+        }
 
-		public static TimePeriodColor fromCode(String code) {
-			for (TimePeriodColor entity : TimePeriodColor.values()) {
-				if (entity.getCode().equals(code)) {
-					return entity;
-				}
-			}
-			return TimePeriodColor.MORNING;
-		}
-	}
+        public static TimePeriodColor fromCode(String code) {
+            for (TimePeriodColor entity : TimePeriodColor.values()) {
+                if (entity.getCode().equals(code)) {
+                    return entity;
+                }
+            }
+            return TimePeriodColor.MORNING;
+        }
+    }
 
 }
